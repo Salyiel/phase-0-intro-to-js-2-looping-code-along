165d0c3523172e89dfbef31d8175dce537d54122
Stacy <salyiela@icloud.com>
2024-03-31 16:48:19 +0300

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let myArray = []
+    for(let c = 0; c< array.length; c++) {
+        let message = `Thank you, ${array[c]}, for the wonderful ${event} gift!`
+        myArray.push(message)
+    }
+    return myArray;
+}
+
+
+function countDown() {
+    let i = 10;
+    while(i > -1){
+        console.log(i);
+        i--
+    }
+}
